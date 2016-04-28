9e44e1710fc4c18daeb6caee8daf27a510480187
Sophia Allen <smlelevich@gmail.com>
2016-04-28 11:18:44 -0700

    first commit

---
+++ SelectService/App_Code/IShowTrackerService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShowTrackerService" in both code and config file together.
+[ServiceContract]
+public interface IShowTrackerService
+{
+    [OperationContract]
+    List<string> GetVenues();
+
+    [OperationContract]
+    List<string> GetArtists();
+
+    [OperationContract]
+    List<string> GetShows();
+
+    [OperationContract]
+    List<Performance> GetVenueShows(string venue);
+
+    [OperationContract]
+    List<Performance> GetArtistShows(string artist);
+}
+
+[DataContract]
+public class Performance
+{
+    [DataMember]
+    public string ShowDate { set; get; }
+
+    [DataMember]
+    public string ShowName { set; get; }
+
+    [DataMember]
+    public string ShowStartTime { set; get; }
+
+    [DataMember]
+    public string ShowVenue { set; get; }
+}
---
+++ SelectService/App_Code/ShowTrackerService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShowTrackerService" in code, svc and config file together.
+public class ShowTrackerService : IShowTrackerService
+{
+    ShowTrackerEntities ste = new ShowTrackerEntities();
+    public List<string> GetArtists()
+    {
+        var arts = from a in ste.Artists
+                      orderby a.ArtistName
+                      select new { a.ArtistName };
+        List<string> artists = new List<string>();
+        foreach(var i in arts)
+        {
+            artists.Add(i.ArtistName.ToString());
+        }
+
+        return artists;
+    }
+
+    public List<Performance> GetArtistShows(string artist)
+    {
+        var artshows = from s in ste.Shows
+                       from sd in s.ShowDetails
+                       where sd.Artist.ArtistName.Equals(artist)
+                       select new { s.Venue.VenueName, s.ShowName, s.ShowDate, s.ShowTime };
+
+        List<Performance> performlist = new List<Performance>();
+        foreach(var sh in artshows)
+        {
+            Performance newshow = new Performance();
+            newshow.ShowDate = sh.ShowDate.ToString();
+            newshow.ShowName = sh.ShowName;
+            newshow.ShowStartTime = sh.ShowTime.ToString();
+            newshow.ShowVenue = sh.VenueName;
+            performlist.Add(newshow);
+        }
+
+        return performlist;
+    }
+
+    public List<string> GetShows()
+    {
+        var shws = from sh in ste.Shows
+                   orderby sh.ShowName
+                   select new { sh.ShowName };
+        List<string> shows = new List<string>();
+        foreach(var s in shws)
+        {
+            shows.Add(s.ShowName);
+        }
+        return shows;
+    }
+
+    public List<string> GetVenues()
+    {
+        var venues = from v in ste.Venues
+                     orderby v.VenueName
+                     select new { v.VenueName };
+        List<string> venuelist = new List<string>();
+        foreach(var ven in venues)
+        {
+            venuelist.Add(ven.VenueName);
+        }
+        return venuelist;
+    }
+
+    public List<Performance> GetVenueShows(string venue)
+    {
+        var venshows = from v in ste.Venues
+                       from s in v.Shows
+                       where s.Venue.VenueName.Equals(venue)
+                       select new
+                       {
+                           s.ShowName,
+                           s.ShowDate,
+                           s.ShowTime,
+                           v.VenueName
+                       };
+        List<Performance> venshowlist = new List<Performance>();
+        foreach(var vs in venshows)
+        {
+            var newvs = new Performance();
+            newvs.ShowName = vs.ShowName;
+            newvs.ShowDate = vs.ShowDate.ToShortDateString();
+            newvs.ShowStartTime = vs.ShowTime.ToString();
+            newvs.ShowVenue = vs.VenueName;
+            venshowlist.Add(newvs);
+        }
+        return venshowlist;
+
+    }
+}
